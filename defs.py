6d58cabd6834017da8a7ef04f4b13049491e8048
Mark Banford <markbanford@gmail.com>
2023-07-15 11:36:09 +0100

    Initial commit

---
+++ defs.py
@@ -0,0 +1,9 @@
+API_KEY = '5da62f3dd4f1970b36b34fd526e3fd73-b5da565754ada6b28d5a7a2bd6c123fc'
+ACCOUNT_ID = '101-004-26341352-001'
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+
+    'Authorization': f'Bearer {API_KEY}'
+
+}
